8c9c985bb738980ea1da91ebd43bba6a0eda7115
Ahsanab002 <ahsan4357067@gmail.com>
2025-10-09 04:25:24 +0500

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ import sharp from 'sharp';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT ?? "5000", 10);
 
 // Middleware
 // Add compression middleware for better performance
